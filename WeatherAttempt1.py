b7ab02652cc65071b453201029e4b467ef7a6a18
queencrab93 <73863529+queencrab93@users.noreply.github.com>
2020-12-06 23:29:24 -0600

    Delete WeatherAttempt1.py
    This needs to be deleted lol

--- WeatherAttempt1.py
+++
@@ -1,8 +0,0 @@
-api.openweathermap.org/data/2.5/weather?q={city name}&appid={
-userapi=a6fcce494ce0c664ed5b48eaa1af926f
-
-
-location=input("Enter the city name of which you would like to view the forecast.")
-
-api_link="api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+userapi
-
